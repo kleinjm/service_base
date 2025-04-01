dcbc509bfc033f0170f59c77308132a052870e7a
James Klein <kleinjm007@gmail.com>
2025-04-01 14:55:12 -0600

    fix rspec loading

--- lib/service_base/rspec.rb
+++ lib/service_base/rspec.rb
@@ -2,5 +2,5 @@
 
 # relative-require all rspec files
 Dir[File.dirname(__FILE__) + '/rspec/*.rb'].each do |file|
-  require 'base_service/rspec/' + File.basename(file, File.extname(file))
+  require 'service_base/rspec/' + File.basename(file, File.extname(file))
 end
