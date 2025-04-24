34ce8736c23c086c4e28fb8842243d47014d4775
James Klein <kleinjm007@gmail.com>
2025-04-24 15:23:06 -0600

    fix type generator

--- lib/generators/types_generator.rb
+++ lib/generators/types_generator.rb
@@ -4,13 +4,13 @@ class TypesGenerator < Rails::Generators::Base
   source_root File.expand_path('templates', __dir__)
 
   def create_types_file
-    types_path = 'app/models/types.rb'
+    types_path = 'app/models/type.rb'
 
     if File.exist?(types_path)
       # File exists, check if it needs the include statement
       content = File.read(types_path)
 
-      unless content.include?('include ServiceBase::Type')
+      unless content.include?('include ServiceBase::Types')
         # Add include statement at the top of the module
         new_content = content.sub(/module Type\s*\n/, "module Type\n  include ServiceBase::Types\n")
         File.write(types_path, new_content)
